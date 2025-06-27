8c47a35f19598b0d7a190b6b50db9bd8b5ffd305
Michael Alexander <michael.lxndr@outlook.com>
2025-06-27 05:40:23 -0500

    second problem

--- Solucion_Codigo/src/P02AlquilerPeliculas.java
+++ Solucion_Codigo/src/P02AlquilerPeliculas.java
@@ -1,4 +1,6 @@
-class Pelicula {
+import java.util.*;
+
+abstract class Pelicula {
     protected String titulo;
     protected String autor;
     protected int anio;
@@ -11,39 +13,50 @@ class Pelicula {
         this.idioma = idioma;
     }
 
-    public double calcularPrecio() {
-        return 0.0;
-    }
-
-    public String toString() {
-        return titulo + " (" + anio + ")";
+    public String datosBaseFormateados() {
+        return String.format("Título: %-25s Autor: %-20s Año: %-4d \t\t Idioma: %-10s",
+                titulo, autor, anio, idioma);
     }
 }
 
 class DVD extends Pelicula {
-    private double capacidadGB;
+    private final double capacidadGB;
+    private final List<String> idiomas;
 
-    public DVD(String titulo, String autor, int anio, String idioma, double capacidadGB) {
-        super(titulo, autor, anio, idioma);
+    public DVD(String titulo, String autor, int anio, List<String> idiomas, double capacidadGB) {
+        super(titulo, autor, anio, idiomas.isEmpty() ? "" : idiomas.getFirst());
+        this.idiomas = new ArrayList<>(idiomas);
         this.capacidadGB = capacidadGB;
     }
 
+    private double calcularPrecio() {
+        // Precio base para VHS: $10, DVD es 10% más caro
+        return 10.0 * 1.10;
+    }
+
     @Override
-    public double calcularPrecio() {
-        return 11.0;
+    public String toString() {
+        return String.format("[DVD] %s Capacidad: %-3.1fGB \t Idiomas: %-30s Precio alquiler: $%-5.2f",
+                datosBaseFormateados(), capacidadGB, idiomas.toString(), calcularPrecio());
     }
 }
 
 class VHS extends Pelicula {
-    private String tipoCinta;
+    private final String tipoCinta;
 
     public VHS(String titulo, String autor, int anio, String idioma, String tipoCinta) {
         super(titulo, autor, anio, idioma);
         this.tipoCinta = tipoCinta;
     }
 
-    @Override
-    public double calcularPrecio() {
+    private double calcularPrecio() {
+        // Precio base para VHS: $10
         return 10.0;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[VHS] %s Tipo de cinta: %-45s Precio alquiler: $%-5.2f",
+                datosBaseFormateados(), tipoCinta, calcularPrecio());
     }
 }
--- Solucion_Codigo/src/P02AlquilerPeliculasEjecutor.java
+++ Solucion_Codigo/src/P02AlquilerPeliculasEjecutor.java
@@ -1,9 +1,22 @@
+import java.util.*;
+
 public class P02AlquilerPeliculasEjecutor {
     public static void main(String[] args) {
-        DVD dvd = new DVD("Matrix", "Wachowski", 1999, "Español/Inglés", 4.7);
-        VHS vhs = new VHS("Titanic", "Cameron", 1997, "Inglés", "Magnetica tipo I");
+        // Crear DVDs
+        List<String> idiomasDVD1 = Arrays.asList("Español", "Inglés", "Francés");
+        Pelicula dvd1 = new DVD("Matrix", "Wachowski", 1999, idiomasDVD1, 4.7);
 
-        System.out.println(dvd + " - Precio: $" + dvd.calcularPrecio());
-        System.out.println(vhs + " - Precio: $" + vhs.calcularPrecio());
+        List<String> idiomasDVD2 = Arrays.asList("Español", "Portugués");
+        Pelicula dvd2 = new DVD("El laberinto del fauno", "Guillermo del Toro", 2006, idiomasDVD2, 8.5);
+
+        // Crear VHS
+        Pelicula vhs1 = new VHS("Jurassic Park", "Spielberg", 1993, "Español", "Cinta magnética tipo I");
+        Pelicula vhs2 = new VHS("Titanic", "James Cameron", 1997, "Inglés", "Cinta magnética tipo II");
+
+        // Mostrar información y precios
+        List<Pelicula> peliculas = Arrays.asList(dvd1, dvd2, vhs1, vhs2);
+        for (Pelicula p : peliculas) {
+            System.out.println(p);
+        }
     }
 }
